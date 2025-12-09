37e571dee2dd27bf6f976348154c8dae69cb51a0
Spaloren <javierf05122005@gmail.com>
2025-12-09 17:36:26 +0100

    Implementación imágenes (En desarrollo?

--- app/src/main/java/com/example/veoveo/conexion/RetrofitClient.kt
+++ app/src/main/java/com/example/veoveo/conexion/RetrofitClient.kt
@@ -10,7 +10,7 @@ object RetrofitClient {
     private const val BASE_URL = "https://api.themoviedb.org/3/"
 
     // Pega aquí tu token largo (el que venía en <<access_token>>)
-    private const val ACCESS_TOKEN = "TU_TOKEN_LARGO_AQUI"
+    private const val ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNjZmZjg3OWZlN2U5NTc1YTExNjVhZTY5YjdiYThjMyIsIm5iZiI6MTc2NTI4Mzg5Mi40NzksInN1YiI6IjY5MzgxODM0NTA3ZjU4NTM2Y2Y3Y2QzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h9zjlpzXNK4Sqlovfp_M-Gqo9le6mVBi2qlR5jPsLzw"
 
     private val client = OkHttpClient.Builder().apply {
         addInterceptor { chain ->
